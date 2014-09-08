62f3026a035d5cc20f36677f3a0a25292054a8b7
Jordan Rogers-Smith <jordanrs@fb.com>
2014-09-08 22:29:56 +0100

    images

--- main.js
+++ main.js
@@ -12,4 +12,26 @@ $(document).ready(function(){
         }
       }
     });
+    
+    $('form').submit(function(e){
+      e.preventDefault();
+
+      $.ajax({
+          url: "//forms.brace.io/emmaandjordangethitched@gmail.com", 
+          method: "POST",
+          data: {coming: "YES", message: "hello!", from: 'blalalala', dietry: "none"},
+          dataType: "json",
+          success: function(){
+            $('#already_rsvp').fadeOut(500);
+            $('#need_to_rsvp').fadeIn(500);
+          }
+      });
+    });
+    
+    $('#show_rsvp').click(function(e){
+      e.preventDefault();
+      $('#already_rsvp').hide();
+      $('#need_to_rsvp').show();
+    })
+    
 })
