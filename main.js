56d002664131b1ba1c063dab0910375b3ff3763a
Jordan Rogers-Smith <jordanrs@fb.com>
2014-09-09 19:04:45 +0100

    final changes

--- main.js
+++ main.js
@@ -1,5 +1,53 @@
+/*********************************************************
+gets the value of a cookie
+**********************************************************/
+document.getCookie = function(sName)
+{
+    sName = sName.toLowerCase();
+    var oCrumbles = document.cookie.split(';');
+    for(var i=0; i<oCrumbles.length;i++)
+    {
+        var oPair= oCrumbles[i].split('=');
+        var sKey = decodeURIComponent(oPair[0].trim().toLowerCase());
+        var sValue = oPair.length>1?oPair[1]:'';
+        if(sKey == sName)
+            return decodeURIComponent(sValue);
+    }
+    return '';
+}
+/*********************************************************
+sets the value of a cookie
+**********************************************************/
+document.setCookie = function(sName,sValue)
+{
+    var oDate = new Date();
+    oDate.setYear(oDate.getFullYear()+1);
+    var sCookie = encodeURIComponent(sName) + '=' + encodeURIComponent(sValue) + ';expires=' + oDate.toGMTString() + ';path=/';
+    document.cookie= sCookie;
+}
+
 $(document).ready(function(){
 	// scroll smooth
+  function showRsvp() {
+    $('#already_rsvp').fadeOut(500, 
+      function(){ 
+        $('#need_to_rsvp').fadeIn(500)
+      }
+    );
+  } 
+  
+    function hideRsvp() {
+      $('#need_to_rsvp').fadeOut(500, 
+        function(){ 
+          $('#already_rsvp').fadeIn(500)
+        }
+      );
+    }
+    
+    if(document.getCookie('rsvpd')){
+      hideRsvp();
+    } 
+    
     $('a[href*=#]:not([href=#])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
         var target = $(this.hash);
@@ -15,23 +63,31 @@ $(document).ready(function(){
     
     $('form').submit(function(e){
       e.preventDefault();
-
-      $.ajax({
-          url: "//forms.brace.io/emmaandjordangethitched@gmail.com", 
-          method: "POST",
-          data: {coming: "YES", message: "hello!", from: 'blalalala', dietry: "none"},
-          dataType: "json",
-          success: function(){
-            $('#already_rsvp').fadeOut(500);
-            $('#need_to_rsvp').fadeIn(500);
+      var to = 'emmaandjordangethitched'
+      
+      if ($(this).parsley().isValid()) {
+        $.ajax({
+            url: "//forms.brace.io/" + to + "@gmail.com", 
+            method: "POST",
+            data: {
+              coming: $(this).find('[name="coming"]:checked').val(),
+              guests: $(this).find('#form_name').val(), 
+              email: $(this).find('#form_email').val(), 
+              comments: $(this).find('#form_comments').val()
+            },
+            dataType: "json",
+            success: function(){
+                hideRsvp;
+                document.setCookie('rsvpd', 1);
+              }.bind(this)
+            });
           }
       });
-    });
     
     $('#show_rsvp').click(function(e){
       e.preventDefault();
-      $('#already_rsvp').hide();
-      $('#need_to_rsvp').show();
+      showRsvp();  
     })
+    
     
 })
